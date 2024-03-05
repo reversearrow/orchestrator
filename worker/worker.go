aec035e4f11fac9820c047263e124933ce938fc1
jayshah <reversearrow@gmail.com>
2024-03-05 10:15:04 -0500

    test pr

--- worker/worker.go
+++ worker/worker.go
@@ -138,6 +138,7 @@ func (w *Worker) GetTasks() []*task.Task {
 	return tasks
 }
 
+// RunTasks runs tasks
 func (w *Worker) RunTasks(ctx context.Context, logger *log.Logger) {
 	const sleep = time.Second * 10
 
